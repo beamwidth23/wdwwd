179b9cd15bd92b00b1038abcbad322e2f994fe58
beamwidth23 <74601392+beamwidth23@users.noreply.github.com>
2023-05-25 09:27:29 +0900

    Update k.cpp

--- wdwwd/k.cpp
+++ wdwwd/k.cpp
@@ -1,4 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std; // int std; plus
+
+int main() {
+int std;
+    srand(time(0)); 
+
+    printf("가위바위보 게임을 시작합니다!");
+
+    while (true) {
+        int computer = rand() % 3; 
+        int player;
+
+        printf("가위(0), 바위(1), 보(2) 중 하나를 선택하세요: ");
+        cin >> player;
+
+        printf("컴퓨터: ");
+        switch (computer) {
+            case 0:
+                cout << "가위" << endl;
+                break;
+            case 1:
+                cout << "바위" << endl;
+                break;
+            case 2:
+                cout << "보" << endl;
+                break;
+        }
+	    
         printf ("플레이어: ");
         switch (player) {
             case 0:
